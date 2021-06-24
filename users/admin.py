b65eefae3c0fed4b688a0e789d72037e98426711
ahn1305 <ahnashwin1305@gmail.com>
2021-06-24 16:48:52 +0530

    added export as csv option to profile model

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Profile
+import csv
+from django.http import HttpResponse
 # Register your models here.
 
 class ProfileAdmin(admin.ModelAdmin):
@@ -12,6 +14,26 @@ class ProfileAdmin(admin.ModelAdmin):
         ('SocialMediaProfile',{'fields':['website_url','instagram_username','facebook_username','twitter_username','github_username','linkedin_username'],
         'classes':['collapse']}),
     ]
+    actions = ["export_as_csv"]
 
+    def export_as_csv(self, request, queryset):
+
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
+
+
+# Adding export as csv option django admin: https://books.agiliq.com/projects/django-admin-cookbook/en/latest/export.html
 
 admin.site.register(Profile,ProfileAdmin)
